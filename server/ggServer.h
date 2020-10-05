fd31364c01707bb11ac3a29221102f5258839063
AndrewsLR <48866053+AndrewsLR@users.noreply.github.com>
2020-10-05 14:31:03 -0300

    Update and rename ggServer to ggServer.h

--- server/ggServer.h
+++ server/ggServer.h
@@ -6,6 +6,7 @@ using namespace std;
 class Group_M{
 	private:
 	map<string,GMServer*> GM_map;
+	mutable shared_timed_mutex mutex;
 	public:
 	vector<Mensagem> ReadMessage(string group);
 	void WriteMessage(Mensagem mensagem);
@@ -18,6 +19,8 @@ class Group_M{
 
 vector<Mensagem>  Group_M::ReadMessage(string group){
 	map<string,GMServer*>::iterator it;
+	
+	unique_lock<shared_timed_mutex> lock(mutex);
 	it = GM_map.find(group);
 	
 	if(it == GM_map.end()){
@@ -25,12 +28,14 @@ vector<Mensagem>  Group_M::ReadMessage(string group){
 		GM_map.insert(pair<string,GMServer*>(group,new_GM));
 		it = GM_map.find(group);
 	}
-
+	unique_lock<shared_timed_mutex> unlock(mutex);
 	return it->second->ReadLastMessages(N);
 }
 
 void Group_M::WriteMessage(Mensagem mensagem){
 	map<string,GMServer*>::iterator it;
+	
+	unique_lock<shared_timed_mutex> lock(mutex);
 	it = GM_map.find(mensagem.grupo);
 	
 	if(it == GM_map.end()){
@@ -38,7 +43,7 @@ void Group_M::WriteMessage(Mensagem mensagem){
 		GM_map.insert(pair<string,GMServer*>(mensagem.grupo,new_GM));
 		it = GM_map.find(mensagem.grupo);
 	}
-
+	unique_lock<shared_timed_mutex> unlock(mutex);
 	it->second->WriteMessage(mensagem);
 	return;
 }
